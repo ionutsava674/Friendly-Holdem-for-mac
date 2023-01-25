96d24602d571c28551fb7d381ef7d6f76377e8d8
Ionut Sava <ionut.sava.8@gmail.com>
2023-01-25 20:37:40 +0200

    added app

---
+++ Friendly Holdem/Friendly_HoldemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friendly_HoldemApp.swift
+//  Friendly Holdem
+//
+//  Created by Ionut Sava on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Friendly_HoldemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
